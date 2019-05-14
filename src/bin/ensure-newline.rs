8acffb596bbd57a5c8b8b832177ba2f546abc84a
Svend Sorensen <svend@svends.net>
2019-05-14 16:55:39 -0700

    Update

--- src/bin/ensure-newline.rs
+++ src/bin/ensure-newline.rs
@@ -1,6 +1,7 @@
 use std::fs::{File, OpenOptions};
 use std::io;
 use std::io::prelude::*;
+use std::io::SeekFrom;
 use std::path::{Path, PathBuf};
 use std::process;
 use structopt::StructOpt;
@@ -40,11 +41,16 @@ fn try_main() -> io::Result<()> {
     Ok(())
 }
 
-fn has_newline<R: io::Read>(mut r: R) -> io::Result<bool> {
-    let mut s = String::new();
-    r.read_to_string(&mut s)?;
+fn has_newline<R: io::Read + io::Seek>(mut r: R) -> io::Result<bool> {
+    let mut buffer = String::new();
+    if r.seek(SeekFrom::End(-1)).is_err() {
+        // seek -1 on empty reader returns "invalid seek to a negative or
+        // overflowing position"
+        return Ok(false);
+    };
+    r.read_to_string(&mut buffer)?;
 
-    match s.chars().last() {
+    match buffer.chars().last() {
         Some('\n') => Ok(true),
         _ => Ok(false),
     }
@@ -66,7 +72,13 @@ mod tests {
         let cursor = io::Cursor::new("one\two\n");
         assert!(has_newline(cursor).unwrap());
 
+        let cursor = io::Cursor::new("\n");
+        assert!(has_newline(cursor).unwrap());
+
         let cursor = io::Cursor::new("one\two");
+        assert!(!has_newline(cursor).unwrap());
+
+        let cursor = io::Cursor::new("");
         assert!(!has_newline(cursor).unwrap());
     }
 }
