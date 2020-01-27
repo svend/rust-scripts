d6555008a67792870a0bc30f995f3991bd1b37b5
Svend Sorensen <svend@svends.net>
2020-01-27 15:18:44 -0800

    Add multiple word lists

--- src/bin/xkcd-password.rs
+++ src/bin/xkcd-password.rs
@@ -1,3 +1,4 @@
+use clap::arg_enum;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 use structopt::StructOpt;
@@ -5,16 +6,37 @@ use structopt::StructOpt;
 /// Generate XKCD 936 password
 #[derive(StructOpt, Debug)]
 struct Opt {
+    /// Word list
+    #[structopt(long = "word-list", default_value = "Bip39")]
+    word_list: WordList,
     /// Minimum bits of entropy
     #[structopt(long = "min-bits", default_value = "44")]
     min_bits: usize,
+}
+
+arg_enum! {
+    #[derive(Debug)]
+    enum WordList {
+        Bip39,
+        EffLarge,
+    }
+}
+
+impl WordList {
+    fn words(&self) -> Vec<String> {
+        let words = match self {
+            Self::Bip39 => include_str!("bip-0039-english.txt"),
+            Self::EffLarge => include_str!("eff_large_wordlist.txt"),
+        };
+        words.lines().map(|s| s.to_string()).collect()
+    }
 }
 
 fn main() {
     let opt = Opt::from_args();
     let min_bits = opt.min_bits;
 
-    let words = get_words();
+    let words = WordList::Bip39.words();
     let length = min_length(words.len(), min_bits);
 
     let mut rng = thread_rng();
@@ -29,11 +51,6 @@ fn main() {
     .join(" ");
 
     println!("{}", password);
-}
-
-fn get_words() -> Vec<String> {
-    let words = include_str!("bip-0039-english.txt");
-    words.lines().map(|s| s.to_string()).collect()
 }
 
 fn min_length(num_symbols: usize, min_bits: usize) -> usize {
