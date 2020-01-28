546717bc5e47e9f31a9336a2a405fa3b7806da90
Svend Sorensen <svend@svends.net>
2020-01-28 13:18:59 -0800

    Sort lines without IP addresses

--- src/bin/sort-ips.rs
+++ src/bin/sort-ips.rs
@@ -1,7 +1,9 @@
 use regex::Regex;
 use std::io::BufRead;
-use std::net::IpAddr;
+use std::net::{IpAddr, Ipv4Addr};
 use std::str::FromStr;
+
+const DEFAULT_IP: IpAddr = IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0));
 
 fn main() {
     let stdin = std::io::stdin();
@@ -17,7 +19,7 @@ fn sort_lines<R: BufRead>(r: R) -> Vec<String> {
         .lines()
         .map(|s| s.expect("failed to get line from stdin"))
         .collect();
-    lines.sort_by_key(|line| get_ip(&line).expect("failed to fine IP address in line"));
+    lines.sort_by_key(|line| get_ip(&line).unwrap_or(DEFAULT_IP));
     lines
 }
 
